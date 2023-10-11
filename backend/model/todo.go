14e727da8d3ab0eea9e8f2e0abffa498829ec604
藤原彰人 <1998922akito@gmail.com>
2023-10-12 00:03:22 +0900

    model file の分割

--- backend/model/todo.go
+++ backend/model/todo.go
@@ -2,9 +2,11 @@ package model
 
 import "time"
 
-type User struct {
+type Todo struct {
 	ID        uint      `json:"id  param:"id""`
 	Name      string    `json:"name"`
+	Task      string    `json:"task"`
+	Done      *bool     `json:"done" gorm:"default:false"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
